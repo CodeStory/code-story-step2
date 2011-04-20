48084610e8699d4b2814ca4dd232016da8e3bf0b
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2011-04-20 22:45:42 +0200

    Initial GildedRose Kata in Java

---
+++ src/test/java/fr/xebia/katas/gildedrose/InnTest.java
@@ -0,0 +1,14 @@
+package fr.xebia.katas.gildedrose;
+
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class InnTest {
+
+   @Test
+   public void should_test_the_truth() throws Exception {
+      assertThat(true).isTrue();
+   }
+
+}
