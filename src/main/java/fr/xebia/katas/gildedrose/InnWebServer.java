208742ed13292c246e93ddf276a608282bd06fd7
David Gageot <david@gageot.net>
2012-09-03 17:30:19 +0200

    Webpage

--- src/main/java/fr/xebia/katas/gildedrose/InnWebServer.java
+++ src/main/java/fr/xebia/katas/gildedrose/InnWebServer.java
@@ -9,11 +9,29 @@ import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 
 public class InnWebServer implements HttpHandler {
-  public void handle(HttpExchange exchange) throws IOException {
-    byte[] response = "Hello World".getBytes();
+  private final Inn inn = new Inn();
 
-    exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, response.length);
-    exchange.getResponseBody().write(response);
+  public void handle(HttpExchange exchange) throws IOException {
+    if ("/update".equals(exchange.getRequestURI().getPath())) {
+      inn.updateQuality();
+
+      exchange.getResponseHeaders().add("Location", "/");
+      exchange.sendResponseHeaders(HttpURLConnection.HTTP_SEE_OTHER, 0);
+    } else {
+      StringBuilder page = new StringBuilder("<!DOCTYPE html><html lang=\"en\"><body><table>");
+
+      for (Item item : inn.getItems()) {
+        page.append(String.format("<tr><td>%s</td><td>%s</td><td>%s</td></tr>", item.getName(), item.getQuality(), item.getSellIn()));
+      }
+
+      page.append("</table><form action=\"/update\"><input type=\"submit\"></form></body>");
+
+      byte[] response = page.toString().getBytes();
+
+      exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, response.length);
+      exchange.getResponseBody().write(response);
+    }
+
     exchange.close();
   }
 
